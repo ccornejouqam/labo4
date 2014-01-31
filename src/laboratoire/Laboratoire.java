c513cc6285502a379bf4de08dae3d320b137615f
Christian Cornejo <ccornejo@multilearning.com>
2014-01-30 23:31:12 -0500

    toto

--- src/laboratoire/Laboratoire.java
+++ src/laboratoire/Laboratoire.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
 	TEST MY COMMIT : )
-        blalbalba
+        blalbalba toto
 */
 
 package laboratoire;
